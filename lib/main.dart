049530ac82cb46609d77c9d8528c08756761c2c7
sainathhiwale <sainathhiwale2@gmail.com>
2024-04-06 22:13:05 +0530

    Flutter BoxShadow Tutorial with demo code is implemented

--- lib/main.dart
+++ lib/main.dart
@@ -5,30 +5,101 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello World Demo Application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Home pages'),
-    );
-  }
-}
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  static const String _title = 'Flutter Tutorial';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this.title),),
-      body: const Center(
-          child:Image(
-    image: AssetImage('assets/icon.png'))
-    ));
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+        home: Scaffold(
+          appBar: AppBar(title: const Text(_title)),
+          body: MyStatefulWidget(),
+        ),
+    );
+  }
+}
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyStatefulWidgetState();
+
+
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+          children: <Widget>[
+            const SizedBox(height: 20,),
+            Container(
+              height: 150,
+              width: 150,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                      offset: Offset(4, 4)
+                  ),
+                ],
+                color: Colors.green[200],
+              ),
+              child: const Text('Container 1'),
+            ),
+            const SizedBox(height: 20,),
+            Container(
+              height: 150,
+              width: 150,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.blue,
+                      blurRadius: 3,
+                      spreadRadius: 5,
+                      offset: Offset(0, 0)
+                  ),
+                  BoxShadow(
+                      color: Colors.red,
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                      offset: Offset(5, 5)
+                  ),
+                ],
+                color: Colors.green[200],
+              ),
+              child: const Text('Container 2'),
+            ),
+            const SizedBox(height: 20,),
+            Container(
+              height: 150,
+              width: 150,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.deepPurpleAccent,
+                      blurRadius: 10,
+                      spreadRadius: 0,
+                      offset: Offset(0, 0)
+                  ),
+                ],
+                color: Colors.green[200],
+              ),
+              child: const Text('Container 3'),
+            ),
+          ]
+      ),
+    );
   }
 }
 
