a94fd0dae898ce4ff521f6e8bbbee4f11a3d79af
sainathhiwale <sainathhiwale2@gmail.com>
2024-04-09 19:34:27 +0530

    Flutter CircularProgressIndicator Tutorial with demo code is implemented

--- lib/main.dart
+++ lib/main.dart
@@ -3,101 +3,48 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  static const String _title = 'Flutter Tutorial';
-
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-        home: Scaffold(
-          appBar: AppBar(title: const Text(_title)),
-          body: MyStatefulWidget(),
-        ),
-    );
+  _MyAppState createState() => _MyAppState();
+}
+
+
+
+class _MyAppState extends State<MyApp> {
+  Icon fab = Icon(
+    Icons.refresh,
+  );
+
+  bool showProgress = true;
+
+  void toggleSubmitState() {
+    setState(() {
+      showProgress = !showProgress;
+    });
   }
-}
-class MyStatefulWidget extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _MyStatefulWidgetState();
 
-
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-          children: <Widget>[
-            const SizedBox(height: 20,),
-            Container(
-              height: 150,
-              width: 150,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: const [
-                  BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 5,
-                      spreadRadius: 1,
-                      offset: Offset(4, 4)
-                  ),
-                ],
-                color: Colors.green[200],
-              ),
-              child: const Text('Container 1'),
-            ),
-            const SizedBox(height: 20,),
-            Container(
-              height: 150,
-              width: 150,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: const [
-                  BoxShadow(
-                      color: Colors.blue,
-                      blurRadius: 3,
-                      spreadRadius: 5,
-                      offset: Offset(0, 0)
-                  ),
-                  BoxShadow(
-                      color: Colors.red,
-                      blurRadius: 5,
-                      spreadRadius: 1,
-                      offset: Offset(5, 5)
-                  ),
-                ],
-                color: Colors.green[200],
-              ),
-              child: const Text('Container 2'),
-            ),
-            const SizedBox(height: 20,),
-            Container(
-              height: 150,
-              width: 150,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: const [
-                  BoxShadow(
-                      color: Colors.deepPurpleAccent,
-                      blurRadius: 10,
-                      spreadRadius: 0,
-                      offset: Offset(0, 0)
-                  ),
-                ],
-                color: Colors.green[200],
-              ),
-              child: const Text('Container 3'),
-            ),
-          ]
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("Flutter - tutorialkart.com"),
+      ),
+      body: Center(
+          child: showProgress
+              ? CircularProgressIndicator()
+              : Text('Click on Refreseh button below', style: TextStyle(fontSize: 20),)),
+      floatingActionButton: FloatingActionButton(
+        child: fab,
+        onPressed: () => setState(() {
+          showProgress = !showProgress;
+          if (showProgress) {
+            fab = Icon(
+              Icons.stop,
+            );
+          } else {
+            fab = Icon(Icons.refresh);
+          }
+        }),
       ),
     );
   }
