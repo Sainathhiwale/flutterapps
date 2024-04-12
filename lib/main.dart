b751a4b8822cdb728d9e8e71d89b03223bd7ef04
sainathhiwale <sainathhiwale2@gmail.com>
2024-04-12 23:22:21 +0530

    click on tab layout(TabBarView) and bind list in selected tab

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,12 @@ class TabControllerDemo extends StatelessWidget {
             ),
             title: const Text('Tab Demo'),
           ),
-          body: const TabBarView(
+          body: TabBarView(
             children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
+             // Icon(Icons.directions_car),
+              CarList(),
+              TrainNameList(),
+              BikeList(),
             ],
           ),
         ),
@@ -47,6 +48,107 @@ class TabControllerDemo extends StatelessWidget {
     );
   }
 }
+
+class CarList extends StatelessWidget {
+   CarList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: carData.length,
+      itemBuilder: (context,index){
+        return ListTile(
+          title: Text(carData[index]),
+        );
+      },
+    );
+  }
+  // Sample car data
+  final List<String> carData = [
+    'Toyota',
+    'Honda',
+    'Ford',
+    'Chevrolet',
+    'BMW',
+    'Mercedes-Benz',
+    'Audi',
+    'Volkswagen',
+    'Tesla',
+    'Nissan',
+  ];
+}
+
+class TrainNameList extends StatelessWidget {
+   TrainNameList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: trainsList.length,
+      itemBuilder: (context, index){
+        return ListTile(
+          title: Text(trainsList[index]),
+        );
+      },
+    );
+  }
+  final List<String> trainsList = [
+    "Abohar Jodhpur Express",
+    "Agartala Mumbai LTT AC Super Fast Express",
+    "Agartala Rani Kamlapati Special",
+    "Agra Cantt Ahmedabad Super Fast ",
+    "Agartala Mumbai LTT AC Super Fast Express"
+    "Sabarmati Daulatpur Chowk Express",
+    "Sabarmati Shri Mata Vaishno Devi Katra Express",
+    "Sabroom Agartala DEMU Special",
+    "Sadulpur Hanumangarh Passenger Special",
+    "Raxaul Howrah Express",
+    "Raxaul Hyderabad Weekly Express",
+    "Puri Gunupur Express",
+    "Pune Danapur Super Fast Summer Special",
+    "New Delhi Rani Kamlapati Shatabdi Express",
+    "New Delhi - Amritsar Swarna Jayanti Shatabdi Express",
+    "Nangal Dam Ambala Cantonmentmemu Express Special",
+    "Nanjangud - Mysuru Express Special",
+  ];
+}
+
+class BikeList extends StatelessWidget {
+   BikeList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: bikeList.length,
+      itemBuilder: (context, index){
+        return ListTile(
+          title: Text(bikeList[index]),
+        );
+      },
+    );
+  }
+  final List<String> bikeList = [
+   "Hero Splendor Plus",
+   "TVS Raider 125 ",
+   "TVS Apache RTR 160 4V",
+   "Yamaha MT 15 ",
+   "Yamaha FZS Fi V4",
+   "Yamaha R15 V4",
+    "Bajaj Pulsar NS200",
+    "Royal Enfield",
+    "Yamaha",
+    "Honda Activa 6G",
+    "Ampere NXG",
+    "Jawa 350",
+    "Honda NX500",
+    "Royal Enfield Shotgun 650",
+    "Hero Xtreme 125R",
+
+  ];
+}
+
+
+
 //https://docs.flutter.dev/cookbook/design/tabs
 
 
