bcd5abb9950744f4719b2aebaf26b6fa36c707c1
sainathhiwale <sainathhiwale2@gmail.com>
2024-04-12 21:17:35 +0530

    count sample code is added with floatingActionButton and text to display count when pressed the button.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapps/Screen/spash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -7,13 +6,44 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MySplashScreen(),
+      home: MainPage(),
     );
   }
-
 }
+
+class MainPage extends StatefulWidget {
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int num =0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("Count App") ,
+          centerTitle :true,
+      ),
+      body: Center(
+       child: Text("the count is :$num"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            num++;
+          });
+        },
+        backgroundColor: Colors.red,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
