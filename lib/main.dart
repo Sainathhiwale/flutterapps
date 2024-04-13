d0fe8b6dca50482e2532838f79a683f8f789c313
sainathhiwale <sainathhiwale2@gmail.com>
2024-04-14 03:30:36 +0530

    TextEditingController login demo is added

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  get kTransparentImage => null;
 
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Form Styling Demo';
+    const appTitle = 'TextEditingController Form Styling Demo';
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: appTitle,
@@ -27,6 +26,82 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controllers when the widget is disposed
+    emailController.dispose();
+    passwordController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+            child: TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+            ),
+            Padding(padding:
+            EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+            child: TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            ),
+            Padding(padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: double.infinity,
+              child:ElevatedButton(
+                onPressed: () {
+                  String email = emailController.text;
+                  String password = passwordController.text;
+                  // Show a Snackbar
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Data saved:  Email: $email, Password: $password'),
+                    ),
+                  );
+                },
+                child: Text('Save'),
+              ),
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
 class MyCustomForm extends StatelessWidget {
   const MyCustomForm({super.key});
 
@@ -69,3 +144,4 @@ class MyCustomForm extends StatelessWidget {
     );
   }
 }
+*/
