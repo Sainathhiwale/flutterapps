e6572edfcf65d8410ccba5da43f36c084445947e
sainathhiwale <sainathhiwale2@gmail.com>
2024-04-15 17:33:54 +0530

    network image challenge and materialapp and scaffold widget is use

--- lib/main.dart
+++ lib/main.dart
@@ -1,147 +1,26 @@
-import 'dart:html';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    const appTitle = 'TextEditingController Form Styling Demo';
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: const MyCustomForm(),
-      ),
-    );
-  }
-}
-
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  State<MyCustomForm> createState() => _MyCustomFormState();
-}
-
-class _MyCustomFormState extends State<MyCustomForm> {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
-  @override
-  void dispose() {
-    // Clean up the controllers when the widget is disposed
-    emailController.dispose();
-    passwordController.dispose();
-    // TODO: implement dispose
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-            child: TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email',
-              ),
-            ),
-            ),
-            Padding(padding:
-            EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-            child: TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            ),
-            Padding(padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              width: double.infinity,
-              child:ElevatedButton(
-                onPressed: () {
-                  String email = emailController.text;
-                  String password = passwordController.text;
-                  // Show a Snackbar
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Data saved:  Email: $email, Password: $password'),
-                    ),
-                  );
-                },
-                child: Text('Save'),
-              ),
-            ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+     home: Scaffold(
+       backgroundColor: Colors.blueGrey,
+       appBar: AppBar(
+         centerTitle: true,
+         title: Text('Image demo'),
+         backgroundColor: Colors.pink[900],
+       ),
+       body:Center(
+         child: Image(
+           alignment:Alignment.center ,
+           image: NetworkImage('https://t4.ftcdn.net/jpg/05/47/97/81/360_F_547978128_vqEEUYBr1vcAwfRAqReZXTYtyawpgLcC.jpg'),
+         ),
+       ),
+     ),
+    ),
+  );
 }
 
 
-/*
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), hintText: 'enter a name '),
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), hintText: 'enter password'),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            width: double.infinity,
-          child: ElevatedButton(
-            onPressed: () {
-              // Show a Snackbar
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text("Data is saved Successfully!"),
-                ),
-              );
-              },
-            child: Text('Login'),
-          ),
-        ),
-        ),
-      ],
-    );
-  }
-}
-*/
